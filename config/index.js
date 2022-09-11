a84cbf6023610fc8baf3114655d0f295596f513b
chenwenze1 <113252948+chenwenze1@users.noreply.github.com>
2022-09-11 10:39:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -33,9 +33,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "ogGRg6avgLO8S5XUkOd6pNbppZAc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "uCGE5pQTbFyeT4QIbpZOtr5sBotqZGyZeUQwPXXEfgw	",
       // 所在省份
       province: "广东",
       // 所在城市
