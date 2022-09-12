1d4a61c28b6c31422867761e1e98715f89c4d525
chenwenze1 <113252948+chenwenze1@users.noreply.github.com>
2022-09-12 20:18:57 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -24,7 +24,9 @@ export const toLowerLine = (str) => {
 export const getColor = () => {
   return `#${ Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, '0') }`
 }
-
+export const getColor1 = () =>{
+  return `#${(0x00EEEE).toString(16)}`
+}
 /**
  * 生成一个从min 到 max 的随机数
  * @param {*} min
