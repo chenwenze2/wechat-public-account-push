6e59462d1b4c45c9f9e0b5789e967308504f3ff8
chenwenze1 <113252948+chenwenze1@users.noreply.github.com>
2022-09-18 14:41:18 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -47,9 +47,9 @@ export const DEFAULT_OUTPUT = {
   poetryTitle: '静夜思',
   poetryContent: '举头望明月，低头思故乡。',
   constellationFortune: '福星高照! 去争取自己想要的一切吧!',
-  weather: '',
-  maxTemperature: '',
-  minTemperature: '',
+  weather: '心暖向你',
+  maxTemperature: '想你的温度°C',
+  minTemperature: '爱你的温度°C',
   windDirection: '朝向你',
   windScale: '100级哦',
 }
